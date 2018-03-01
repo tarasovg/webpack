cc6c505ee921955e4a0ee289a923a47014b37c54
skoob13 <georgiy@ridgebox.com>
2018-03-01 18:15:07 +0300

    update webpack

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,28 @@
-function build() {
-  require('./build');
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+function getDefaultConfig(c) {
+  return c;
 }
 
-function start() {
-  require('./start');
+function dev(getConfig = getDefaultConfig) {
+  const devSetup = require('./config/dev.setup');
+  const developmentConfig = require('../config/webpack.config.dev');
+  devSetup(getConfig(developmentConfig));
 }
 
-function test() {
-  require('./test');
+function build(getConfig = getDefaultConfig) {
+  const buildSetup = require('./config/build.setup');
+  const productionConfig = require('../config/webpack.config.prod');
+  buildSetup(getConfig(productionConfig));
 }
 
 module.exports = {
   build,
-  start,
-  test,
+  dev,
 };
